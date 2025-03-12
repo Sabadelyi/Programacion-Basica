1e24e73df0f34d64bdfbf6e1367c0241175b4f4b
Sabadelyi <sabaydjesusmcolpz@gmail.com>
2025-03-12 07:53:42 -0600

    12/03/2025

--- Unidad 3/Hackaton3.1.py
+++ Unidad 3/Hackaton3.1.py
@@ -1,7 +1,21 @@
-# Lista para guardar los artículos
-articulos = []
+# Preguntar si deseas realizar el mandado
+respuesta = input("¿Deseas realizar el mandado? (sí/no): ")
 
-# Pregunta inicial
-respuesta = input("¿Quieres comenzar con el mandado?")
+if respuesta.lower() == "sí":
+    # Preguntar cuántos artículos desea comprar
+    cantidad = int(input("¿Cuántos artículos deseas comprar?: "))
 
-if respuesta == "sí":
+    # Crear una lista para guardar los artículos
+    lista_articulos = []
+
+    # Pedir los nombres de los artículos
+    for i in range(cantidad):
+        articulo = input(f"Ingrese el nombre del artículo {i + 1}: ")
+        lista_articulos.append(articulo)
+
+    # Imprimir los artículos
+    print("\nLos artículos que deseas comprar son:")
+    for articulo in lista_articulos:
+        print(f"- {articulo}")
+else:
+    print("¡Tal vez en otro momento!")
---
+++ Unidad 3/Listas4.py
@@ -0,0 +1,27 @@
+# Lista nombres
+nombres = ["Cristian", "Enrique", "Oswaldo", "Roberto", "Adal", "Alejandro", "Manuel", "Aaron", "Lupita", "Alexa"]
+
+# Lista edades
+edades = [18, 18, 18, 18, 18, 18, 18, 18, 18, 18]
+
+# Lista materias
+materias = ["Programacion Basica", "Calculo Diferencial", "Algebra lineal", "Contabilidad", 
+            "Estadistica y control de Calidad", "Materiales", "Ingles"]
+
+# Lista maestros
+maestros = ["Enrique", "Eduardo", "Marlen", "Cecilia", "Jorge", "Martha"]
+
+print(len(nombres)) 
+print(max(edades))  
+print(min(edades))  
+nombres.sort()  
+print(nombres)
+
+
+materias.remove("Programacion Basica")  
+print(materias)
+materias.append("Programacion Avanzada")  
+materias.append([1, "Programacion Basica"])  
+print(materias)
+for Materias in materias:
+	print(Materias)

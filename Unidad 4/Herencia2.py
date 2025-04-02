5c1533882612b914621605e330635821a08c50ee
Sabadelyi <sabaydjesusmcolpz@gmail.com>
2025-04-02 08:04:03 -0600

    02/04/25

---
+++ Unidad 4/Herencia2.py
@@ -0,0 +1,11 @@
+
+
+class Dispositivos tecnologicos :
+    def __init__(self, componentes, tamaño, peso, funcion):
+        self.componentes = componentes
+        self.tamaño = tamaño
+        self.peso = peso
+        self.funcion = funcion
+
+         def funciones(self):
+        return f"Tiene un buen prosesador {self.componentes} tambien el tamaño no es tan grande {self.tamaño} el peso que tiene "
